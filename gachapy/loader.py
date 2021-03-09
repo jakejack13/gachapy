7e5deb8cbd80b5a0fa68eb432fb37d20ccd8a36b
Jacob Kerr <jck268@cornell.edu>
2021-03-09 11:52:26 -0500

    Removed gachapy file to reduce confusion between files

--- gachapy/loader.py
+++ gachapy/loader.py
@@ -1,6 +1,10 @@
 """Collection of loader functions for loading gachapy objects from json files
 
 Functions
+load_controller(items_filename,banners_filename,players_filename) : Controller
+    Creates a Controller object from the specified json files
+save_controller(controller,items_filename,banners_filename,players_filename) : None
+    Saves the controller in json format into the specified files
 load_items_from_file(filename,controller) : List[Optional[Item]]
     Load items into controller from the specified json file
 load_banners_from_file(filename,controller) : List[Optional[Banner]]
@@ -12,6 +16,56 @@ load_players_from_file(filename,controller) : List[Optional[Player]]
 from gachapy.objects import *
 from gachapy.controller import *
 import json
+
+def load_controller(items_filename,banners_filename,players_filename) -> Controller :
+    """Creates a Controller object from the specified json files
+
+    Parameters
+    items_filename : str
+        the path of the items json file
+    banners_filename : str
+        the path of the banners json file
+    players_filename : str
+        the path of the players json file
+    
+    Returns
+    Controller
+        the controller loaded from the specified files
+    """
+    controller = Controller()
+    load_items_from_file(items_filename,controller)
+    load_banners_from_file(banners_filename,controller)
+    load_players_from_file(players_filename,controller)
+    return controller
+
+def save_controller(controller,items_filename,banners_filename,players_filename) -> None :
+    """Saves the controller in json format into the specified files
+
+    Parameters
+    controller : Controller 
+        the controller to save into files
+    items_filename : str
+        the path of the items json file
+    banners_filename : str
+        the path of the banners json file
+    players_filename : str
+        the path of the players json file
+
+    Return
+    None
+    """
+    items_list = controller.items
+    item_dict = {"items": [{"name":i.name,"description":i.description,"rarity":i.rarity} for i in items_list]}
+    with open(items_filename, "w") as f:
+        json.dump(item_dict,f)
+    banners_list = controller.banners
+    banner_dict = {"banners": [{"name":i.name,"items":[{"name":j.name} for j in i.item_list],"modifier":i.modifier,"price":i.price}] for i in banners_list}
+    with open(banners_filename, "w") as f:
+        json.dump(banner_dict,f)
+    players_list = controller.players
+    player_dict = {"players": [{"name":i.name,"money":i.money,"items":[{"name":j.name} for j in i.items]} for i in players_list]}
+    with open(players_filename, "w") as f:
+        json.dump(player_dict,f)
 
 def load_items_from_file(filename,controller) -> List[Optional[Item]] :
     """Load items into controller from the specified json file
