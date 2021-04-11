feb0a069d59c0837a481ef2404b424bae523d00b
Jacob Kerr <jck268@cornell.edu>
2021-04-11 01:07:09 -0400

    Added dark theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@ import os
 import sys
 import subprocess
 
-subprocess.run(["pip", "install", "groundwork-sphinx-theme"])
-
 sys.path.insert(0, os.path.abspath(".."))
 
 
@@ -48,7 +46,15 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "groundwork"
+
+subprocess.run(["pip", "install", "sphinx_pdj_theme"])
+import sphinx_pdj_theme
+
+html_theme = "sphinx_pdj_theme"
+htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+
+# sets the darker appearence
+html_theme_options = {"style": "darker"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
